bbba0b94a1d025fc76c3c211d83a025cde864de9
Joelmukono <jlmukono@gamil.com>
2019-12-09 13:38:08 +0300

    add url and token environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:"https://api.github.com/users",
+  apiToken:"?e6ba2d44c3a8b673db9500eae10cdf192b286c7c",
 };
 
 /*
