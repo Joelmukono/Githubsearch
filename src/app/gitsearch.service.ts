9c9d6f0d3c3280437dbf58fd8414b725ef37213c
Joelmukono <jlmukono@gamil.com>
2019-12-09 14:00:27 +0300

    generate search component

--- src/app/gitsearch.service.ts
+++ src/app/gitsearch.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import  {User} from './profile';
+import { HttpClient } from '@angular/common/http';
+import {environment } from '../environments/environment';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitsearchService {
 
-  constructor() { }
+
+  user:User;
+
+  constructor(private http:HttpClient) {
+    this.user = new User("","","","");
+   }
+
+   userRequest(username){
+     interface ApiResponse{
+       name:string;
+       login:string;
+       avatar_url:string;
+       repos_url:string;
+     }
+     let promise = new Promise((resolve,reject)=>{
+       this.http.get<ApiResponse>(environment.apiUrl+username+environment.apiToken).toPromise().then(response=>{
+         this.user.name = response.name;
+         this.user.login = response.name;
+         this.user.avatar_url = response.name;
+         this.user.repos_url = response.repos_url;
+
+         resolve()
+
+       },error=>{
+         this.user.name = "error"
+         this.user.login ="error"
+         this.user.avatar_url="error"
+         this.user.repos_url = "error"
+
+         reject(error)
+
+
+
+       })
+     })
+     return promise
+
+
+   }
+
+   
 }
--- src/app/profile.ts
+++ src/app/profile.ts
@@ -1,5 +1,5 @@
 export class User {
-    constructor(public name:string,public login:string,public avatar_url:string,private repos_url:string){
+    constructor(public name:string,public login:string,public avatar_url:string,public repos_url:string){
 
     }
 }
