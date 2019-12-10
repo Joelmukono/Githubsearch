601bffec22f36d226a87aed75b41b51aa850a3ef
Joelmukono <jlmukono@gamil.com>
2019-12-10 07:06:58 +0300

    add routing in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SearchComponent} from './search/search.component';
+import {LandingComponent}from './landing/landing.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'search',component:SearchComponent },
+  {path:'Home',component:LandingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
