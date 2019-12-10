a8cff35fc8359342dc33c026170228c3aabc274c
Joelmukono <jlmukono@gamil.com>
2019-12-10 07:55:40 +0300

    add navigation to nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SearchComponent} from './search/search.component';
 import {LandingComponent}from './landing/landing.component';
+import {NotFoundComponent} from './not-found/not-found.component';
 
 const routes: Routes = [
   {path:'search',component:SearchComponent },
   {path:'home',component:LandingComponent},
+  { path:'**', component:NotFoundComponent},
+
+  { path: '', redirectTo:"/goals", pathMatch:"full"},
 ];
 
 @NgModule({
