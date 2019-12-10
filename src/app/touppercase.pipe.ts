72caa6441a46bff6709dcc236c0ab70e4cb640a8
Joelmukono <jlmukono@gamil.com>
2019-12-10 08:42:18 +0300

    implement custom pipe

--- src/app/touppercase.pipe.ts
+++ src/app/touppercase.pipe.ts
@@ -5,8 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TouppercasePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value:string): any {
+    value.toLocaleUpperCase();
+    return value;
   }
 
 }
